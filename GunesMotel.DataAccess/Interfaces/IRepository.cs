a7d0f9262f0ae11ad1258170e51d3466ed21e5ee
ayseeaydin <ayssee.aydinn.04@gmail.com>
2025-05-06 21:07:55 +0300

    Commit

---
+++ GunesMotel.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GunesMotel.DataAccess.Interfaces
+{
+    // IRepository: Generic CRUD işlemlerini temsil eden arayüz
+    public interface IRepository<T> where T : class
+    {
+        // Tüm kayıtları getir
+        List<T> GetAll();
+
+        // ID ile tek kayıt getir
+        T? GetById(int id);
+
+        // Yeni kayıt ekle
+        void Add(T entity);
+
+        // Var olan kaydı güncelle
+        void Update(T entity);
+
+        // ID ile kaydı sil
+        void Delete(int id);
+    }
+}
